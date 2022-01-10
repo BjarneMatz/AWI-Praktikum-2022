88102ad930c70d6b0b093f8922652039b2daabd9
BjarneMatz <bjarne.matz@gmail.com>
2022-01-10 14:56:36 +0100

    day 1 final

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def getData(sensor_id):
 def viewMeta(sensor_id):
     pass
 
+def sensorActive():
+
+
 #open root window
 root = Tk()
 root.title("SENSOR Metadata Writer")
@@ -19,11 +22,15 @@ root.geometry("720x280")
 header = ttk.Frame(root, padding="10 10 10 10")
 footer = ttk.Frame(root, padding="10 10 10 10")
 sensorinfo = ttk.Frame(root, padding="10 10 10 10")
-eventdata = ttk.Frame(root, padding="10 10 10 10")
+data = ttk.Frame(root, padding="10 10 10 10")
 upload = ttk.Frame(root, padding="10 10 10 10")
 
+
+sensorid = ttk.Frame(sensorinfo, padding="10 10 10 10").grid(column=1, row=0)
+sensormeta = ttk.Frame(sensorinfo, padding="10 10 10 10").grid(column=1, row=1)
+
 sensorinfo.grid(column=0, row=1, sticky=(N, W, E, S))
-eventdata.grid(column=1, row=1, sticky=(N, W, E, S))
+data.grid(column=1, row=1, sticky=(N, W, E, S))
 upload.grid(column=2, row=1, sticky=(N, W, E, S))
 
 header.grid(column=0, row=0, sticky=(N, W, E, S))
@@ -33,10 +40,11 @@ footer.grid(column=0, row=2, sticky=(N, W, E, S))
 ttk.Label(header, text="SENSOR Metadata Writer").grid(column=0, row=0, sticky=(N, W, E, S))
 
 #----SENSOR INFORMATION----
+ttk.Label(sensorinfo, text="Sensor Metadata")
 ttk.Label(sensorinfo, text="Sensor ID:").grid(column=4, row=2)
 id = StringVar()
 ttk.Entry(sensorinfo, width=10, text="id", textvariable=id, state=NORMAL).grid(column=5, row=2)
-
+ttk.Button(sensorinfo, text="Get Sensor Data", width=30).grid(column=4, columnspan=2, row=3)
 ttk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=5, row=5)
 ttk.Label(sensorinfo, text="URN").grid(column=4, row=5)
 ttk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=5, row=6)
@@ -46,9 +54,32 @@ ttk.Label(sensorinfo, text="Long Name").grid(column=4, row=7)
 
 ttk.Button(sensorinfo, text="view more metadata", command=viewMeta(id), state=DISABLED).grid(column=4, columnspan=2, row=8)
 
+#----UPLOAD----
+log = StringVar()
+ttk.Entry(upload, textvariable=log).grid(column=1, row=2)
+ttk.Button(upload, text="Upload", command=uploadData()).grid(column=1, row=1)
+
+
+#----DATA INPUT----
+ttk.Label(data, text="Data2Write").grid(column=1, row=0, columnspan=2)
+input1 = StringVar()
+ttk.Entry(data, textvariable=input1, state=DISABLED).grid(column=1, row=1)
+ttk.Label(data, text="Data1:").grid(column=0, row=1)
+input1 = StringVar()
+ttk.Entry(data, textvariable=input1, state=DISABLED).grid(column=1, row=2)
+ttk.Label(data, text="Data2:").grid(column=0, row=2)
+input1 = StringVar()
+ttk.Entry(data, textvariable=input1, state=DISABLED).grid(column=1, row=3)
+ttk.Label(data, text="Data3:").grid(column=0, row=3)
+input1 = StringVar()
+ttk.Entry(data, textvariable=input1, state=DISABLED).grid(column=1, row=4)
+ttk.Label(data, text="Data4:").grid(column=0, row=4)
+input1 = StringVar()
+ttk.Entry(data, textvariable=input1, state=DISABLED).grid(column=1, row=5)
+ttk.Label(data, text="Data5:").grid(column=0, row=5)
 
 #----FOOTER----
-ttk.Button(upload, text="Upload", command=uploadData()).grid(column=1, row=2)
+
 
 #main program loop
 root.mainloop()
