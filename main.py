ca7cbdd7ec9732b7ffb5065759f2db8a69e1d8c6
BjarneMatz <bjarne.matz@gmail.com>
2022-01-10 14:15:24 +0100

    header, footer, 3 columns

--- main.py
+++ main.py
@@ -2,48 +2,53 @@ from tkinter import *
 from tkinter import ttk
 
 
-def ex():
-    try:
-        ide = float(id.get())
-        greatid.set(int(ide + 1))
-    except ValueError:
-        print("Falsch.")
+def uploadData():
+    pass
 
+def getData(sensor_id):
+    pass
+def viewMeta(sensor_id):
+    pass
+
+#open root window
 root = Tk()
+root.title("SENSOR Metadata Writer")
+root.geometry("720x280")
 
-root.title("Hallo Welt")
+#set frames of main window
+header = ttk.Frame(root, padding="10 10 10 10")
+footer = ttk.Frame(root, padding="10 10 10 10")
+sensorinfo = ttk.Frame(root, padding="10 10 10 10")
+eventdata = ttk.Frame(root, padding="10 10 10 10")
+upload = ttk.Frame(root, padding="10 10 10 10")
 
-mainframe = ttk.Frame(root, padding="200 200 200 200")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+sensorinfo.grid(column=0, row=1, sticky=(N, W, E, S))
+eventdata.grid(column=1, row=1, sticky=(N, W, E, S))
+upload.grid(column=2, row=1, sticky=(N, W, E, S))
 
+header.grid(column=0, row=0, sticky=(N, W, E, S))
+footer.grid(column=0, row=2, sticky=(N, W, E, S))
 
-ttk.Label(mainframe, text="Hallo Welt").grid(column=1, row=1)
-ttk.Button(mainframe, text="Tschüss!", command=ex).grid(column=1, row=2)
+#----HEADER----
+ttk.Label(header, text="SENSOR Metadata Writer").grid(column=0, row=0, sticky=(N, W, E, S))
 
-
+#----SENSOR INFORMATION----
+ttk.Label(sensorinfo, text="Sensor ID:").grid(column=4, row=2)
 id = StringVar()
-id_entry = ttk.Entry(mainframe, width=10, textvariable=id)
-id_entry.grid(column=1, row=3)
+ttk.Entry(sensorinfo, width=10, text="id", textvariable=id, state=NORMAL).grid(column=5, row=2)
 
-greatid = StringVar()
-ttk.Label(mainframe, textvariable=greatid).grid(column=2, row=1)
+ttk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=5, row=5)
+ttk.Label(sensorinfo, text="URN").grid(column=4, row=5)
+ttk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=5, row=6)
+ttk.Label(sensorinfo, text="Short Name").grid(column=4, row=6)
+ttk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=5, row=7)
+ttk.Label(sensorinfo, text="Long Name").grid(column=4, row=7)
+
+ttk.Button(sensorinfo, text="view more metadata", command=viewMeta(id), state=DISABLED).grid(column=4, columnspan=2, row=8)
 
 
+#----FOOTER----
+ttk.Button(upload, text="Upload", command=uploadData()).grid(column=1, row=2)
+
+#main program loop
 root.mainloop()
-
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
