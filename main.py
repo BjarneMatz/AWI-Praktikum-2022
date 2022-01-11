779c924abac50285750b31c37b13ef5dd867a313
BjarneMatz <bjarne.matz@gmail.com>
2022-01-11 10:46:24 +0100

    implemented basic http and json interaction

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 from tkinter import *
 import tkinter as tk
+import json, requests
+
 
 
 def uploadData():
     pass
 
 
-def getData(sensorid):
-    pass
+def getData():
+    try:
+        sid = id.get()
+        sensor = requests.get(f"https://sandbox.sensor.awi.de/rest/sensors/device/getDevice/{sid}")
+        sensor = sensor.json()
+        urnget = sensor["urn"]
+        shortnameget = sensor["shortName"]
+        longnameget = sensor["longName"]
 
 
-def viewMeta(sensor_id):
+        changeText(urn, urnget)
+        changeText(shortname, shortnameget)
+        changeText(longname, longnameget)
+        metabutton.config(state=NORMAL)
+    except Exception as ex:
+        changeText(logger, ex)
+
+def changeText(box, string):
+    box.config(state=NORMAL)
+    box.insert(0, string)
+    box.config(state=DISABLED)
+def viewMeta():
     pass
 
 
@@ -21,7 +40,7 @@ def sensorActive():
 # set root window
 root = tk.Tk()
 root.title("SENSOR Metadata Writer")
-# root.geometry("720x280")
+root.geometry("1280x720")
 root.config(bg="#07ace7")
 
 # set frames of main window
@@ -47,22 +66,27 @@ tk.Label(sensorinfo, text="Sensor Metadata").grid(column=0, row=0, columnspan=2)
 tk.Label(sensorinfo, text="Sensor ID:").grid(column=0, row=2)
 id = StringVar()
 tk.Entry(sensorinfo, width=10, textvariable=id, state=NORMAL).grid(column=1, row=2)
-tk.Button(sensorinfo, text="Load Sensor", width=30).grid(column=0, columnspan=2, row=3)
+tk.Button(sensorinfo, text="Load Sensor", width=30, command=getData).grid(column=0, columnspan=2, row=3)
 
 tk.Label(sensorinfo, text="Selected Sensor:").grid(column=0, row=4, columnspan=2)
-tk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=1, row=5)
+urn = tk.Entry(sensorinfo, width=80, state=DISABLED)
+urn.grid(column=1, row=5)
 tk.Label(sensorinfo, text="URN").grid(column=0, row=5)
-tk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=1, row=6)
+shortname = tk.Entry(sensorinfo, width=80, state=DISABLED)
+shortname.grid(column=1, row=6)
 tk.Label(sensorinfo, text="Short Name").grid(column=0, row=6)
-tk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=1, row=7)
+longname = tk.Entry(sensorinfo, width=80, state=DISABLED)
+longname.grid(column=1, row=7)
 tk.Label(sensorinfo, text="Long Name").grid(column=0, row=7)
 
-tk.Button(sensorinfo, text="view more metadata", command=viewMeta(id), state=DISABLED).grid(column=0, columnspan=2,row=8)
+metabutton = tk.Button(sensorinfo, text="view more metadata", command=viewMeta, state=DISABLED)
+metabutton.grid(column=0, columnspan=2,row=8)
 
 # ----UPLOAD----
 log = StringVar()
-tk.Entry(upload, textvariable=log).grid(column=1, row=2)
-tk.Button(upload, text="Upload", command=uploadData()).grid(column=1, row=1)
+logger = tk.Entry(upload, state=DISABLED)
+logger.grid(column=1, row=2)
+tk.Button(upload, text="Upload", command=uploadData).grid(column=1, row=1)
 
 # ----DATA INPUT----
 tk.Label(data, text="Data2Write").grid(column=1, row=0, columnspan=2)
