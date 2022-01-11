e8c072d84b85b3c79db431c7162e3ee9664190a6
BjarneMatz <bjarne.matz@gmail.com>
2022-01-11 09:25:19 +0100

    changed to tk instad of tkk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 
 
 def uploadData():
@@ -11,48 +11,54 @@ def viewMeta(sensor_id):
     pass
 
 def sensorActive():
+    pass
 
-
-#open root window
-root = Tk()
+#set root window
+root = tk.Tk()
 root.title("SENSOR Metadata Writer")
-root.geometry("720x280")
+#root.geometry("720x280")
+root.config(bg="#07ace7")
 
 #set frames of main window
-header = ttk.Frame(root, padding="10 10 10 10")
-footer = ttk.Frame(root, padding="10 10 10 10")
-sensorinfo = ttk.Frame(root, padding="10 10 10 10")
-data = ttk.Frame(root, padding="10 10 10 10")
-upload = ttk.Frame(root, padding="10 10 10 10")
+header = tk.Frame(root)
+footer = tk.Frame(root)
+sensorinfo = tk.Frame(root)
+data = tk.Frame(root)
+upload = tk.Frame(root)
 
+header.config(bg="#07acff")
 
-sensorid = ttk.Frame(sensorinfo, padding="10 10 10 10").grid(column=1, row=0)
-sensormeta = ttk.Frame(sensorinfo, padding="10 10 10 10").grid(column=1, row=1)
-
-sensorinfo.grid(column=0, row=1, sticky=(N, W, E, S))
+sensorinfo.grid(column=0, row=0, sticky=(N, W, E, S))
 data.grid(column=1, row=1, sticky=(N, W, E, S))
 upload.grid(column=2, row=1, sticky=(N, W, E, S))
 
 header.grid(column=0, row=0, sticky=(N, W, E, S))
 footer.grid(column=0, row=2, sticky=(N, W, E, S))
 
+sensorid = ttk.Frame(sensorinfo, padding="10 10 10 10").grid(column=1, row=0)
+sensormeta = ttk.Frame(sensorinfo, padding="10 10 10 10").grid(column=1, row=1)
+
+
+
 #----HEADER----
 ttk.Label(header, text="SENSOR Metadata Writer").grid(column=0, row=0, sticky=(N, W, E, S))
 
-#----SENSOR INFORMATION----
-ttk.Label(sensorinfo, text="Sensor Metadata")
-ttk.Label(sensorinfo, text="Sensor ID:").grid(column=4, row=2)
+#----SENSOR ID----
+ttk.Label(sensorinfo, text="Sensor Metadata").grid(column=0, row=0, columnspan=2)
+ttk.Label(sensorinfo, text="Sensor ID:").grid(column=0, row=2)
 id = StringVar()
-ttk.Entry(sensorinfo, width=10, text="id", textvariable=id, state=NORMAL).grid(column=5, row=2)
-ttk.Button(sensorinfo, text="Get Sensor Data", width=30).grid(column=4, columnspan=2, row=3)
-ttk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=5, row=5)
-ttk.Label(sensorinfo, text="URN").grid(column=4, row=5)
-ttk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=5, row=6)
-ttk.Label(sensorinfo, text="Short Name").grid(column=4, row=6)
-ttk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=5, row=7)
-ttk.Label(sensorinfo, text="Long Name").grid(column=4, row=7)
+ttk.Entry(sensorinfo, width=10, text="id", textvariable=id, state=NORMAL).grid(column=1, row=2)
+ttk.Button(sensorinfo, text="Load Sensor", width=30).grid(column=0, columnspan=2, row=3)
 
-ttk.Button(sensorinfo, text="view more metadata", command=viewMeta(id), state=DISABLED).grid(column=4, columnspan=2, row=8)
+ttk.Label(sensorinfo, text="Selected Sensor:").grid(column=0, row=4, columnspan=2)
+ttk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=1, row=5)
+ttk.Label(sensorinfo, text="URN").grid(column=0, row=5)
+ttk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=1, row=6)
+ttk.Label(sensorinfo, text="Short Name").grid(column=0, row=6)
+ttk.Entry(sensorinfo, width=20, state=DISABLED).grid(column=1, row=7)
+ttk.Label(sensorinfo, text="Long Name").grid(column=0, row=7)
+
+ttk.Button(sensorinfo, text="view more metadata", command=viewMeta(id), state=DISABLED).grid(column=0, columnspan=2, row=8)
 
 #----UPLOAD----
 log = StringVar()
