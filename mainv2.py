b31c78203879a70ef6e913f3a0955927a62eb47c
BjarneMatz <bjarne.matz@gmail.com>
2022-01-12 14:52:32 +0100

    day 3 final

--- mainv2.py
+++ mainv2.py
@@ -3,9 +3,9 @@ import logging
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
-
+from tkcalendar import *
 import requests
-
+import re
 
 def searchbyid():
     """this function searches for a sensor by its id"""
@@ -20,6 +20,74 @@ def searchbyid():
         setMeta(sensor)
     except Exception as ex:
         print(ex)
+
+def selectdate(self, time=0):
+    if self == "start":
+        date = Toplevel(root)
+        date.title("Select Date")
+        date.geometry("600x400")
+        Label(date, text="Select event start date").pack()
+        cal = Calendar(date, selectmode="day")
+        cal.pack()
+
+        hour_string = ''
+        min_string = ''
+        sec_string = ''
+
+
+        hour = ttk.Spinbox(
+            date,
+            from_=0,
+            to=23,
+            wrap=True,
+            textvariable=hour_string,
+            width=2,
+            state="readonly",
+            justify=CENTER
+        )
+        minute = Spinbox(
+            date,
+            from_=0,
+            to=59,
+            wrap=True,
+            textvariable=min_string,
+            width=2,
+            justify=CENTER
+        )
+
+        second = Spinbox(
+            date,
+            from_=0,
+            to=59,
+            wrap=True,
+            textvariable=sec_string,
+            width=2,
+            justify=CENTER
+        )
+        hour.pack()
+        minute.pack()
+        second.pack()
+
+        h_string = hour.get()
+        m_string = minute.get()
+        s_string = second.get()
+
+        if len(hour.get()) < 2:
+            h_string = str(0) + hour.get()
+        if len(minute.get()) < 2:
+            m_string = str(0) + minute.get()
+        if len(second.get()) < 2:
+            s_string = str(0) + second.get()
+
+        timestring = f"{h_string}:{m_string}:{s_string}"
+
+        Button(date, text="Set Date", command=lambda: selectdate(cal.get_date(), timestring)).pack()
+    if self == "end":
+        pass
+    else:
+        print(self, time)
+        pass
+
 
 
 def setMeta(sensor):
@@ -92,6 +160,9 @@ root.title("Event Writer")
 root.geometry("1280x720")
 root.config(bg="#07ace7")
 
+style = ttk.Style()
+style.theme_use("clam")
+
 # sensor frame is the information and select screen of the sensor
 senframe = Frame(root)
 senframe.place(x=0, y=0, height=360, width=640, bordermode="inside")
@@ -145,24 +216,39 @@ tw.bind('<ButtonRelease-1>', selectItem)
 
 finalevent = str
 
-def seteventtype(self):
+def geteventinfo():
     pass
 
+
+startisend = ''
+
+
+def samedate():
+    global startisend
+    print(c_samedate_state.get())
+    if c_samedate_state.get() == 0:
+        enddateentry.config(state=NORMAL)
+        startisend = False
+    if c_samedate_state.get() == 1:
+        enddateentry.config(state=DISABLED)
+        startisend = True
+
 evframe = Frame(root)
-evframe.place(x=640, y=0, height=360, width=640)
+evframe.place(x=640, y=0, height=720, width=640)
 
 events = requests.get("https://sandbox.sensor.awi.de/rest/sensors/events/getAllEventTypes")
 events = events.json()
 clicked = StringVar()
 clicked.set("Select Event")
 possibevents = []
+
 for ix, ev in enumerate(events):
     print(ix)
     print(ev["generalName"])
     possibevents.append(ev["generalName"] + " (" + str(ev["id"]) + ")")
 
 Label(evframe, text="Event Data").place(x=0, y=0)
-dd = tk.OptionMenu(evframe, clicked, *possibevents, command=seteventtype)
+dd = tk.OptionMenu(evframe, clicked, *possibevents)
 print(possibevents)
 dd.place(x=210, y=50, width=250)
 
@@ -172,15 +258,72 @@ Label(evframe, text="Longitude").place(x=0, y=110)
 Label(evframe, text="Latitude").place(x=0, y=135)
 Label(evframe, text="Elevation").place(x=0, y=160)
 
-
 inlabel = Entry(evframe)
 inlabel.place(x=80, y=60)
 indescription = Entry(evframe)
 indescription.place(x=80, y=85)
 
+#Button(evframe, text="Set Start Date", command=lambda: selectdate("start")).place(x=100, y=200)
+startdateentry = Entry(evframe)
+startdateentry.place(x=380, y=110, width=130)
+enddateentry = Entry(evframe)
+enddateentry.place(x=380, y=135, width=130)
+Label(evframe, text="Start Date (format sensetive!): ").place(x=200, y=110)
+Label(evframe, text="End Date (format sensetive!): ").place(x=200, y=135)
+c_samedate_state = Variable()
+c_samedate = Checkbutton(evframe, variable=c_samedate_state, text="Same start and end date", onvalue=1, offvalue=0, command=samedate)
+c_samedate.place(x=200, y=160)
+c_samedate.deselect()
 
 
 upframe = Frame(root)
-upframe.place(x=0, y=360, width=1280, height=360)
+upframe.place(x=0, y=360, width=640, height=360)
+
+eventdata = {
+    "deviceid": "",
+    "startdate": "",
+    "enddate": "",
+    "desctiption": "",
+    "label": "",
+    "eventtype": "",
+    "longitude": "",
+    "latitude": "",
+    "elevation": ""
+}
+
+Label(upframe, text="Following data will be uploaded to SENSOR:")
+Label(upframe, text="DeviceID: ")
+Label(upframe, text="StartDate: ")
+Label(upframe, text="EndDate ")
+Label(upframe, text="Description ")
+Label(upframe, text="Label: ")
+Label(upframe, text="EventType ")
+Label(upframe, text="Longitude: ")
+Label(upframe, text="Latitude: ")
+Label(upframe, text="Elevation: ")
+
+
+
+
+"""
+def get_eventdata():
+    eventdata = {
+                "itemID": sensor["id"],
+                "inheritToAllChildren": "false",
+                "inheritToChildren" : [],
+                "event": {
+                    "startDate":operation["timestamp"],
+                    "endDate":operation["timestamp"],
+                    "label": operation["label"],
+                    "description": operation["comment"],
+                    "eventType":  device_operation_ids[operation["action"]],
+                    "latitude":operation["latitude"],
+                    "longitude":operation["longitude"],
+                    "elevationInMeter": operation["altitude"],
+                    "id": None
+                }
+"""
+
+
 
 root.mainloop()
