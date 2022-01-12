23f8a02294d124f15e4ba62872f1b136137b2712
BjarneMatz <bjarne.matz@gmail.com>
2022-01-12 11:56:59 +0100

    added basic event input gui

--- mainv2.py
+++ mainv2.py
@@ -1,13 +1,14 @@
-from tkinter import *
-import tkinter as tk
-from tkinter import ttk
 import json
-import requests
 import logging
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+import requests
 
 
-def searchById():
-    "this function searches for a sensor by its id"
+def searchbyid():
+    """this function searches for a sensor by its id"""
     with open('accounts.json', encoding='utf-8') as f:
         accounts = json.load(f)
     try:
@@ -22,7 +23,7 @@ def searchById():
 
 
 def setMeta(sensor):
-    "this function sets the meta information in the sensor information frame to the current sensor used"
+    """this function sets the meta information in the sensor information frame to the current sensor used"""
     isenid.delete(0, "end")
     iurn.delete(0, "end")
     ishortname.delete(0, "end")
@@ -34,7 +35,7 @@ def setMeta(sensor):
 
 
 def selectItem(a):
-    "this functions returns the id of the sensor selected in the treeview menu"
+    """"this functions returns the id of the sensor selected in the treeview menu"""
     item = tw.focus()
     item = tw.item(item)
     if item["values"] == "":
@@ -42,11 +43,11 @@ def selectItem(a):
     else:
         senid.delete(0, "end")
         senid.insert(0, item["values"])
-        searchById()
+        searchbyid()
 
 
 def login():
-    "login code by maximilian betz; used to login on sensor api service"
+    """login code by maximilian betz; used to login on sensor api service"""
 
     with open('accounts.json', encoding='utf-8') as f:
         accounts = json.load(f)
@@ -85,27 +86,24 @@ def login():
         logging.error("Communication error to sensor.awi.de: {}".format(e))
 
 
-
-
-#defining the root window
+# defining the root window
 root = tk.Tk()
 root.title("Event Writer")
 root.geometry("1280x720")
 root.config(bg="#07ace7")
 
-
-#sensor frame is the information and select screen of the sensor
+# sensor frame is the information and select screen of the sensor
 senframe = Frame(root)
 senframe.place(x=0, y=0, height=360, width=640, bordermode="inside")
 Label(senframe, text="Sensor Event Manager").grid(column=0, row=0)
 tw = ttk.Treeview(senframe, columns="c1", cursor="hand1")
-tw.place(x=0 ,y=25, width=200, height=330)
+tw.place(x=0, y=25, width=200, height=330)
 tw.heading(column="c1", text="Collections")
 
 twsb = ttk.Scrollbar(senframe, orient="vertical", command=tw.yview, cursor="double_arrow")
 twsb.place(x=200, y=25, width=20, height=330, anchor=NE)
 
-collections = requests.get(f"https://sandbox.sensor.awi.de/rest/sensors/collections/getAllCollections")
+collections = requests.get("https://sandbox.sensor.awi.de/rest/sensors/collections/getAllCollections")
 collections = collections.json()
 
 for ix, col in enumerate(collections):
@@ -117,7 +115,7 @@ for ix, col in enumerate(collections):
     for iix, item in enumerate(items):
         tw.insert(main, index=iix, text=item["shortName"], values=item["id"])
 
-searchidbutton = Button(senframe, command=searchById, width=20, text="Search by ID")
+searchidbutton = Button(senframe, command=searchbyid, width=20, text="Search by ID")
 searchidbutton.place(x=500, y=0, width=130)
 senid = Entry(senframe, width=40, borderwidth=3)
 senid.place(x=200, y=0, width=300)
@@ -138,14 +136,51 @@ ishortname.place(x=300, y=95, width=300)
 ilongname = Entry(senframe)
 ilongname.place(x=300, y=120, width=300)
 
-#login frame
+# login frame
 loginframe = Frame(root)
 loginframe.grid(column=0, row=0)
 
-#default button binds
+# default button binds
 tw.bind('<ButtonRelease-1>', selectItem)
 
+finalevent = str
+
+def seteventtype(self):
+    pass
+
 evframe = Frame(root)
-evframe.place(x=360, y=0, height=640, width=360)
+evframe.place(x=640, y=0, height=360, width=640)
+
+events = requests.get("https://sandbox.sensor.awi.de/rest/sensors/events/getAllEventTypes")
+events = events.json()
+clicked = StringVar()
+clicked.set("Select Event")
+possibevents = []
+for ix, ev in enumerate(events):
+    print(ix)
+    print(ev["generalName"])
+    possibevents.append(ev["generalName"] + " (" + str(ev["id"]) + ")")
+
+Label(evframe, text="Event Data").place(x=0, y=0)
+dd = tk.OptionMenu(evframe, clicked, *possibevents, command=seteventtype)
+print(possibevents)
+dd.place(x=210, y=50, width=250)
+
+Label(evframe, text="Label").place(x=0, y=60)
+Label(evframe, text="Description").place(x=0, y=85)
+Label(evframe, text="Longitude").place(x=0, y=110)
+Label(evframe, text="Latitude").place(x=0, y=135)
+Label(evframe, text="Elevation").place(x=0, y=160)
+
+
+inlabel = Entry(evframe)
+inlabel.place(x=80, y=60)
+indescription = Entry(evframe)
+indescription.place(x=80, y=85)
+
+
+
+upframe = Frame(root)
+upframe.place(x=0, y=360, width=1280, height=360)
 
 root.mainloop()
