0c0ef3fc3bdc8829d3a650affe79e8b021081ead
BjarneMatz <bjarne.matz@gmail.com>
2022-01-13 10:45:35 +0100

    put everything into functions

--- mainv2.py
+++ mainv2.py
@@ -1,22 +1,24 @@
 import json
 import logging
-
-
-
 import tkinter as tk
 from tkinter import ttk
-#from tkcalendar import *
+# from tkcalendar import *
 import requests
-#import re
+# import re
+
+# global variables
+
+sensor = '' # sensor object from api (json)
+startisend = bool
+
+
 
 def searchbyid():
     """this function searches for a sensor by its id"""
-    with open('accounts.json', encoding='utf-8') as f:
-        accounts = json.load(f)
     try:
         sid = senid.get()
         global sensor
-        url = accounts['sensorurl'] + f'/sensors/device/getDevice/{sid}'
+        url = f'https://sandbox.sensor.awi.de/rest/sensors/device/getDevice/{sid}'
         sensor = requests.get(url)
         sensor = sensor.json()
         setMeta(sensor)
@@ -37,13 +39,13 @@ def setMeta(sensor):
 
 def selectItem(a):
     """"this functions returns the id of the sensor selected in the treeview menu"""
-    item = tw.focus()
-    item = tw.item(item)
-    if item["values"] == "":
+    i = tw.focus()
+    i = tw.item(i)
+    if i["values"] == "":
         pass
     else:
         senid.delete(0, "end")
-        senid.insert(0, item["values"])
+        senid.insert(0, i["values"])
         searchbyid()
 
 
@@ -87,73 +89,87 @@ def login():
         logging.error("Communication error to sensor.awi.de: {}".format(e))
 
 
-# defining the root window
-root = tk.Tk()
-root.title("Event Writer")
-root.geometry("1280x720")
-root.config(bg="#07ace7")
-
-style = ttk.Style()
-style.theme_use("clam")
-
-# sensor frame is the information and select screen of the sensor
-senframe = ttk.Frame(root)
-senframe.place(x=0, y=0, height=360, width=640, bordermode="inside")
-ttk.Label(senframe, text="Sensor Event Manager").grid(column=0, row=0)
-tw = ttk.Treeview(senframe, columns="c1", cursor="hand1")
-tw.place(x=0, y=25, width=200, height=330)
-tw.heading(column="c1", text="Collections")
-
-twsb = ttk.Scrollbar(senframe, orient="vertical", command=tw.yview, cursor="double_arrow")
-twsb.place(x=200, y=25, width=20, height=330, anchor="ne")
-
-collections = requests.get("https://sandbox.sensor.awi.de/rest/sensors/collections/getAllCollections")
-collections = collections.json()
-
-for ix, col in enumerate(collections):
-    print(ix)
-    print(col["collectionName"])
-    items = requests.get(f"https://sandbox.sensor.awi.de/rest/sensors/collections/getItemsOfCollection/{col['id']}")
-    items = items.json()
-    main = tw.insert('', index=ix, text=col["collectionName"])
-    for iix, item in enumerate(items):
-        tw.insert(main, index=iix, text=item["shortName"], values=item["id"])
-
-searchidbutton = ttk.Button(senframe, command=searchbyid, width=20, text="Search by ID")
-searchidbutton.place(x=500, y=0, width=130)
-senid = ttk.Entry(senframe, width=40)
-senid.place(x=200, y=0, width=300)
-
-ttk.Label(senframe, text="Data of selcted sensor").place(x=200, y=25)
-
-ttk.Label(senframe, text="SensorID:").place(x=210, y=45)
-ttk.Label(senframe, text="URN:").place(x=210, y=70)
-ttk.Label(senframe, text="Shortname:").place(x=210, y=95)
-ttk.Label(senframe, text="Longname:").place(x=210, y=120)
-
-isenid = ttk.Entry(senframe)
-isenid.place(x=300, y=45, width=300)
-iurn = ttk.Entry(senframe)
-iurn.place(x=300, y=70, width=300)
-ishortname = ttk.Entry(senframe)
-ishortname.place(x=300, y=95, width=300)
-ilongname = ttk.Entry(senframe)
-ilongname.place(x=300, y=120, width=300)
-
-# login frame
-loginframe = ttk.Frame(root)
-loginframe.grid(column=0, row=0)
-
-# default button binds
-tw.bind('<ButtonRelease-1>', selectItem)
-finalevent = str
-
-def geteventinfo():
+def getupdate():
+    global upid
+    upid.insert(0, "")
+    upid.insert(0, sensor["id"])
     pass
 
+# defining the root window
+def buildroot():
+    global root
+    root = tk.Tk()
+    root.title("Event Writer")
+    root.geometry("1280x720")
+    root.config(bg="#07ace7")
 
-startisend = bool
+    style = ttk.Style()
+    style.theme_use("clam")
 
+# sensor frame is the information and select screen of the sensor
+def buildsenframe():
+    senframe = ttk.Frame(root)
+    senframe.place(x=0, y=0, height=360, width=640, bordermode="inside")
+
+    ttk.Label(senframe, text="Sensor Event Manager").grid(column=0, row=0)
+    global tw
+    tw = ttk.Treeview(senframe, columns="c1", cursor="hand1")
+    tw.place(x=0, y=25, width=200, height=330)
+    tw.heading(column="c1", text="Collections")
+
+    #scrollbar for treeview
+    twsb = ttk.Scrollbar(senframe, orient="vertical", command=tw.yview, cursor="double_arrow")
+    twsb.place(x=200, y=25, width=20, height=330, anchor="ne")
+
+    # get all collections through api
+
+
+    searchidbutton = ttk.Button(senframe, command=searchbyid, width=20, text="Search by ID")
+    searchidbutton.place(x=500, y=0, width=130)
+    global senid
+    senid = ttk.Entry(senframe, width=40)
+    senid.place(x=200, y=0, width=300)
+    senid.bind("<Return>", searchbyid())
+
+    # meta information about selected sensor
+    ttk.Label(senframe, text="Data of selcted sensor").place(x=200, y=25)
+    ttk.Label(senframe, text="SensorID:").place(x=210, y=45)
+    ttk.Label(senframe, text="URN:").place(x=210, y=70)
+    ttk.Label(senframe, text="Shortname:").place(x=210, y=95)
+    ttk.Label(senframe, text="Longname:").place(x=210, y=120)
+
+    global isenid
+    global iurn
+    global ishortname
+    global ilongname
+    isenid = ttk.Entry(senframe)
+    isenid.place(x=300, y=45, width=300)
+    iurn = ttk.Entry(senframe)
+    iurn.place(x=300, y=70, width=300)
+    ishortname = ttk.Entry(senframe)
+    ishortname.place(x=300, y=95, width=300)
+    ilongname = ttk.Entry(senframe)
+    ilongname.place(x=300, y=120, width=300)
+
+    # make it possible to select items in treeview menu
+    tw.bind('<ButtonRelease-1>', selectItem)
+
+def getcollections():
+    collections = requests.get("https://sandbox.sensor.awi.de/rest/sensors/collections/getAllCollections")
+    collections = collections.json()
+
+    for ix, col in enumerate(collections):
+        print(ix)
+        print(col["collectionName"])
+        #get sensors from api
+        items = requests.get(f"https://sandbox.sensor.awi.de/rest/sensors/collections/getItemsOfCollection/{col['id']}")
+        items = items.json()
+        main = tw.insert('', index=ix, text=col["collectionName"])
+        for iix, item in enumerate(items):
+            tw.insert(main, index=iix, text=item["shortName"], values=item["id"])
+
+"""
+# part of old time picker; maybe reusable
 
 def samedate():
     global startisend
@@ -164,38 +180,54 @@ def samedate():
     if c_samedate_state.get() == 1:
         enddateentry.config(state=tk.DISABLED)
         startisend = True
+"""
+def buildevent():
+# event input frame
+    evframe = ttk.Frame(root)
+    evframe.place(x=640, y=0, height=720, width=640)
 
-evframe = ttk.Frame(root)
-evframe.place(x=640, y=0, height=720, width=640)
+# get all possible events from api
+    events = requests.get("https://sandbox.sensor.awi.de/rest/sensors/events/getAllEventTypes")
+    events = events.json()
+    clicked = tk.StringVar()
+    clicked.set("Select Event")
+    possibevents = []
+    for ix, ev in enumerate(events):
+        print(ix)
+        print(ev["generalName"])
+        possibevents.append(ev["generalName"] + " (" + str(ev["id"]) + ")")
+    ttk.Label(evframe, text="Event Data").place(x=0, y=0)
 
-events = requests.get("https://sandbox.sensor.awi.de/rest/sensors/events/getAllEventTypes")
-events = events.json()
-clicked = tk.StringVar()
-clicked.set("Select Event")
-possibevents = []
 
-for ix, ev in enumerate(events):
-    print(ix)
-    print(ev["generalName"])
-    possibevents.append(ev["generalName"] + " (" + str(ev["id"]) + ")")
+#dropdown for possible events from api
+    dd = ttk.OptionMenu(evframe, clicked, *possibevents)
+    dd.place(x=210, y=50, width=250)
 
-ttk.Label(evframe, text="Event Data").place(x=0, y=0)
-dd = tk.OptionMenu(evframe, clicked, *possibevents)
-print(possibevents)
-dd.place(x=210, y=50, width=250)
+# input entry box description
+    ttk.Label(evframe, text="Label:").place(x=0, y=60)
+    ttk.Label(evframe, text="Description:").place(x=0, y=200)
+    ttk.Label(evframe, text="Longitude:").place(x=0, y=110)
+    ttk.Label(evframe, text="Latitude:").place(x=0, y=135)
+    ttk.Label(evframe, text="Elevation:").place(x=0, y=160)
 
-ttk.Label(evframe, text="Label").place(x=0, y=60)
-ttk.Label(evframe, text="Description").place(x=0, y=85)
-ttk.Label(evframe, text="Longitude").place(x=0, y=110)
-ttk.Label(evframe, text="Latitude").place(x=0, y=135)
-ttk.Label(evframe, text="Elevation").place(x=0, y=160)
+#event input boxes
+    inlabel = ttk.Entry(evframe)
+    inlabel.place(x=80, y=60)
+    inlongintude = ttk.Entry(evframe)
+    inlongintude.place(x=80, y=110)
+    inlatitude = ttk.Entry(evframe)
+    inlatitude.place(x=80, y=135)
+    inelevation = ttk.Entry(evframe)
+    inelevation.place(x=80, y=160)
 
-inlabel = ttk.Entry(evframe)
-inlabel.place(x=80, y=60)
-indescription = ttk.Entry(evframe)
-indescription.place(x=80, y=85)
+    indescription = tk.Text(evframe, font=("Calibri 10"))
+    indescription.place(x=80, y=200, width=400, height=300)
+    global upid
+    indescription.bind("<Any-KeyPress>", getupdate())
+"""
+# old time function; maybe reusable
 
-#Button(evframe, text="Set Start Date", command=lambda: selectdate("start")).place(x=100, y=200)
+Button(evframe, text="Set Start Date", command=lambda: selectdate("start")).place(x=100, y=200)
 startdateentry = ttk.Entry(evframe)
 startdateentry.place(x=380, y=110, width=130)
 enddateentry = ttk.Entry(evframe)
@@ -206,10 +238,52 @@ c_samedate_state = tk.Variable()
 c_samedate = ttk.Checkbutton(evframe, variable=c_samedate_state, text="Same start and end date", onvalue=1, offvalue=0, command=samedate)
 c_samedate.place(x=200, y=160)
 #c_samedate.deselect()
+"""
 
+def buildupload():
+    global upframe
+    upframe = ttk.Frame(root)
+    upframe.place(x=0, y=360, width=640, height=360)
 
-upframe = ttk.Frame(root)
-upframe.place(x=0, y=360, width=640, height=360)
+    ttk.Label(upframe, text="Following data will be uploaded to SENSOR:").place(x=0, y=0)
+    ttk.Label(upframe, text="DeviceID: ").place(x=0, y=25)
+    ttk.Label(upframe, text="StartDate: ").place(x=0, y=50)
+    ttk.Label(upframe, text="EndDate: ").place(x=0, y=75)
+    ttk.Label(upframe, text="Description: ").place(x=0, y=100)
+    ttk.Label(upframe, text="Label: ").place(x=0, y=125)
+    ttk.Label(upframe, text="EventType: ").place(x=0, y=150)
+    ttk.Label(upframe, text="Longitude: ").place(x=0, y=175)
+    ttk.Label(upframe, text="Latitude: ").place(x=0, y=200)
+    ttk.Label(upframe, text="Elevation: ").place(x=0, y=225)
+
+    global upid
+    global upstart
+    global upend
+    global updesc
+    global uplab
+    global uptype
+    global uplong
+    global uplat
+    global upel
+
+    upid = ttk.Entry(upframe)
+    upid.place(x=75, y=25, width=300)
+    upstart = ttk.Entry(upframe)
+    upstart.place(x=75, y=50, width=300)
+    upend = ttk.Entry(upframe)
+    upend.place(x=75, y=75, width=300)
+    updesc = ttk.Entry(upframe)
+    updesc.place(x=75, y=100, width=300)
+    uplab = ttk.Entry(upframe)
+    uplab.place(x=75, y=125, width=300)
+    uptype = ttk.Entry(upframe)
+    uptype.place(x=75, y=150, width=300)
+    uplong = ttk.Entry(upframe)
+    uplong.place(x=75, y=175, width=300)
+    uplat = ttk.Entry(upframe)
+    uplat.place(x=75, y=200, width=300)
+    upel = ttk.Entry(upframe)
+    upel.place(x=75, y=225, width=300)
 
 eventdata = {
     "deviceid": "",
@@ -222,20 +296,6 @@ eventdata = {
     "latitude": "",
     "elevation": ""
 }
-
-ttk.Label(upframe, text="Following data will be uploaded to SENSOR:")
-ttk.Label(upframe, text="DeviceID: ")
-ttk.Label(upframe, text="StartDate: ")
-ttk.Label(upframe, text="EndDate ")
-ttk.Label(upframe, text="Description ")
-ttk.Label(upframe, text="Label: ")
-ttk.Label(upframe, text="EventType ")
-ttk.Label(upframe, text="Longitude: ")
-ttk.Label(upframe, text="Latitude: ")
-ttk.Label(upframe, text="Elevation: ")
-
-
-
 
 """
 def get_eventdata():
@@ -256,6 +316,14 @@ def get_eventdata():
                 }
 """
 
+#build guis
+buildroot()
+buildsenframe()
+buildevent()
+buildupload()
+
+#get data from api
+getcollections()
 
 
 root.mainloop()
