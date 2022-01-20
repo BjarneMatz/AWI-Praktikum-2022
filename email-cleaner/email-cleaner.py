3169e4595143279192c2255c595d3650e22b36b8
BjarneMatz <bjarne.matz@gmail.com>
2022-01-20 14:48:42 +0100

    added logging messages

--- email-cleaner/email-cleaner.py
+++ email-cleaner/email-cleaner.py
@@ -4,7 +4,7 @@ import datetime
 from datetime import timedelta
 import email
 import email.utils
-import time
+
 
 class Log:
     def __init__(self):
@@ -24,7 +24,7 @@ class Log:
 class Cleaner:
     def __init__(self):
         self.login()
-        self.scan()
+        self.scananddelete()
         self.imap.close()
         self.imap.logout()
 
@@ -38,6 +38,7 @@ class Cleaner:
                 self.password = account['password']
                 self.server = account['server']
                 self.port = account['port']
+                log.print("Setting user credentials and server information...")
             except:
                 log.print("You need to set your login credentials in account.json!")
             log.print(f"Logging in as {self.username} on {self.server}...")
@@ -48,32 +49,41 @@ class Cleaner:
             except Exception as ex:
                 log.print("Login failed!")
                 log.print(ex)
-    def scan(self):
-        """List all possible folders of target client"""
+    def scananddelete(self):
+        #select no specific folder
         self.imap.select()
-
+        log.print("Removed folder selection...")
+        # set date til mails get removed
         now = datetime.datetime.today()
+        log.print("Set date to now...")
         with open('account.json', encoding='utf-8') as f:
             account = json.load(f)
             maxtime = account['max-time-in-days']
+            log.print("Loaded max duration from json...")
         then = now - timedelta(days=int(maxtime))
         then = then.date()
-        print(then)
         testtime = then.strftime('%d-%b-%Y')
-        print(testtime)
+        log.print("Calculated max duration...")
+        log.print(testtime)
 
         _, mails = self.imap.search(None, f'before "{testtime}"')
+        log.print("Mail search completed...")
         print("Mail IDs : {}\n".format(mails[0].decode().split())) # print all mail ids that have been found
 
         for mail_id in mails[0].decode():
+            log.print(f"Decoding mail id {mail_id}...")
             response, mail_data = self.imap.fetch(mail_id, '(RFC822)')
-            self.message = email.message_from_bytes(mail_data[0][1])
-
-
+            self.message = email.message_from_bytes(mail_data[0])
             subject = (self.message.get("subject"))
+            log.print(f"Found {subject}...")
             print("Deleting", subject)
-            #self.imap.store(mail_id, "+FLAGS", "\\Deleted")
-
+            self.imap.store(mail_id, "+FLAGS", "\\Deleted")
+            log.print(f"Flagged {mail_id} as deleted...")
+        log.print("Looped through all mails successfully...")
+        #delete all mails that are flagged as \\Deleted as set above
+        self.imap.expunge()
+        log.print("Deleted all mails flagged as deleted...")
+        log.print("Exiting now...")
 
 if __name__ == "__main__":
     log = Log()
