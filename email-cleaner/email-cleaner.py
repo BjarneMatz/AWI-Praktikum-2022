af69dc9b9e9d92ac0aacd5408c08d75786579a14
BjarneMatz <bjarne.matz@gmail.com>
2022-01-20 14:11:39 +0100

    working time search function

--- email-cleaner/email-cleaner.py
+++ email-cleaner/email-cleaner.py
@@ -1,6 +1,10 @@
 import imaplib
 import json
-
+import datetime
+from datetime import timedelta
+import email
+import email.utils
+import time
 
 class Log:
     def __init__(self):
@@ -20,10 +24,13 @@ class Log:
 class Cleaner:
     def __init__(self):
         self.login()
+        self.scan()
+        self.imap.close()
+        self.imap.logout()
 
 
     def login(self):
-
+        """Login to mailserver in account.json"""
         with open('account.json', encoding='utf-8') as f:
             account = json.load(f)
             try:
@@ -41,6 +48,28 @@ class Cleaner:
             except Exception as ex:
                 log.print("Login failed!")
                 log.print(ex)
+    def scan(self):
+        """List all possible folders of target client"""
+        self.imap.select()
+
+        now = datetime.datetime.today()
+        then = now - timedelta(days=90)
+        then = then.date()
+        print(then)
+        testtime = then.strftime('%d-%b-%Y')
+        print(testtime)
+
+        _, mails = self.imap.search(None, f'before "{testtime}"')
+        print("Mail IDs : {}\n".format(mails[0].decode().split())) # print all mail ids that have been found
+
+        for mail_id in mails[0].decode():
+            response, mail_data = self.imap.fetch(mail_id, '(RFC822)')
+            self.message = email.message_from_bytes(mail_data[0][1])
+
+
+            subject = (self.message.get("subject"))
+            print("Deleting", subject)
+            #self.imap.store(mail_id, "+FLAGS", "\\Deleted")
 
 
 if __name__ == "__main__":
